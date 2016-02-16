bf024a89e8022d2fbbab1a6fe62899b9f7172c3a
parkercatalano <student@epicodus.com>
2016-02-15 20:36:24 -0800

    add html form & 5 Q's

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,4 @@ var destinations = ["Bahamas", "Italy", "Paris", "Costa Rica", "Belgium"]
 //to combine arrays: var_name.concat//
 //to add elements to array: var_name.push//
 //to access element in array: var_name[0]/[2] etc.//
+//methods like pop(), reverse(), and push()//
