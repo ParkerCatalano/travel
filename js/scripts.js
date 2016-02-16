1354152274e87cc2791e20bb0298cdb8e7c748a7
parkercatalano <student@epicodus.com>
2016-02-15 19:37:02 -0800

    add some content to js establishing an array

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var destinations = ["Bahamas", "Italy", "Paris", "Costa Rica", "Belgium"]
+
+//to combine arrays: var_name.concat//
+//to add elements to array: var_name.push//
+//to access element in array: var_name[0]/[2] etc.//
