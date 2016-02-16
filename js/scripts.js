177a3a51d5dbab81e85e37703e0fad4dcf46ae23
parkercatalano <student@epicodus.com>
2016-02-15 21:00:42 -0800

    fill out questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
-var destinations = ["Bahamas", "Italy", "Paris", "Costa Rica", "Belgium"]
+//var destinations = ["Bahamas", "Italy", "Paris", "Costa Rica", "Belgium"]
 
 //to combine arrays: var_name.concat//
 //to add elements to array: var_name.push//
 //to access element in array: var_name[0]/[2] etc.//
 //methods like pop(), reverse(), and push()//
+
+$(document).ready(function() {
+  $('#blanks form').submit(function() {
+    var blanks = ["question1", "question2", "question3", "question4", "question5"];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+
+    });
+
+    $('#result').show();
+
+    event.preventDefault();
+  });
+});
