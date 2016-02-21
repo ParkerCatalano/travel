950717332a9f4ae6eaac8774040312b2dceb55aa
ParkerCatalano <parkercatalano@gmail.com>
2016-02-20 16:06:38 -0800

    add a new jumbotron picture and expand on conditionals

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,23 @@ $(document).ready(function() {
   $favShop = $('#favShop')
   $gender = $('#gender')
 
-  $("button").click(function() {
+  $('button').click(function() {
 
-    if($favPast.val() === true) {
-      $("#dest2").show();
+    if(($favPast.val() === true)||($favShop.val() === true)){
+      $('#dest1').show();
+    }
+    else if ($favFood.val() === true) {
+      $('#dest2'|| '#dest1').show();
+    }
+    else {
+      $('#dest3').show();
     }
     // clear inputs when you're all done
     $favPast.empty()
     $favWeath.empty()
     $favShop.empty()
     $favFood.empty()
+
+    event.preventDefault();
   })
 })
